841160ba6d8b80e5885fa47809b13004ea419a1e
michal struzek <mstruzek@>
2022-07-19 19:03:53 +0200

    komentarz do interfacu

---
+++ src/main/java/com/mstruzek/msketch/solver/jni/GpuGeometricSolverImpl.java
@@ -0,0 +1,158 @@
+package com.mstruzek.msketch.solver.jni;
+
+import com.mstruzek.msketch.solver.GeometricSolver;
+import com.mstruzek.msketch.solver.SolverStat;
+
+public class GpuGeometricSolverImpl implements GeometricSolver {
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public SolverStat solveSystem(SolverStat solverStat) {
+        return null;
+    }
+}
+
+
+/*
+
+public class GpuGeometricSolverImpl implements GeometricSolver {
+    */
+/*** convergence limit *//*
+
+    private StateReporter reporter;
+
+    @Override
+    public void setup() {
+
+        /// Arena.isSynchronized() -- czy model rezyduje na GPU ?
+
+        /// Arena.synchronizeFromModel()   ---  do GPU or empty
+        /// Arena.synchronizedToModel()     --- od GPU or empty
+
+        /// SYNC: prealokowac pamiec do przodu po kazdje zmianie modelu jesli trzeba < upperBound
+
+        // final MatrixDouble A;, Fq, Wq,Hs, b
+
+        // ---SYNC: PointLocation.setup();
+    }
+
+    @Override
+    public SolverStat solveSystem(SolverStat solverStat) {
+
+        /// Uklad rownan liniowych  [ A * x = b ] powstały z linerazycji ukladu dynamicznego
+
+        final MatrixDouble A;
+        final MatrixDouble Fq;              /// Macierz sztywnosci ukladu obiektow zawieszonych na sprezynach.
+        final MatrixDouble Wq;              /// d(FI)/dq - Jacobian Wiezow
+        final MatrixDouble Hs;
+
+        // Wektor prawych stron [Fr; Fi]'
+        final MatrixDouble b;
+
+        if (dbPoint.size() == 0) {
+            reporter.writeln("[warning] - empty model");
+            return solverStat;
+        }
+        /// JAVA
+        if (Constraint.dbConstraint.isEmpty()) {
+            reporter.writeln("[warning] - no constraint configuration applied ");
+            return solverStat;
+        }
+
+        solverStat.startTime = 0; /// z C20 uzupelaniamy caly stan, to odbiorca zapytujee na koniec sesji !.
+
+        // --- nativeMethod -> (`kernel`):  GeometricPrimitive.getAllJacobianForces(Fq);
+
+/// Wektor prawych stron b
+        MatrixDouble dmx = null;
+        /// State Vector - zmienne stanu
+
+        // -- state vector na koncu z rezultatem --> wciagnijmy do CPU RAM i przewaluowac !
+        // MatrixDouble SV = MatrixDouble.matrix1D(dimension, 0.0);
+        PointUtility.copyIntoStateVector(SV);
+        //:PointUtility.setupLagrangeMultipliers(SV);
+
+        /// petla while jest W JNI nie instrumentujemy obiektu Native
+
+/// C20 :{
+        int itr = 0;
+        while (itr < MAX_SOLVER_ITERATIONS) {
+
+            /// `cublas`
+            A.reset(0.0);
+
+            /// `kernel`
+            GeometricPrimitive.getAllForce(Fr);                 /// Sily  - F(q)
+            Constraint.getFullConstraintValues(Fi);             /// Wiezy  - Fi(q)
+            // `cublas`
+            b.dot(-1);
+
+            // 'kernel'
+            Constraint.getFullJacobian(Wq);                     /// Jq = d(Fi)/dq
+
+            // 'cublas'
+            Hs.reset(0.0);
+
+            // 'kernel'
+            Constraint.getFullHessian(Hs, SV, size);
+
+            // `cublas` block copy
+            A.setSubMatrix(0, 0, Fq);
+            A.addSubMatrix(0, 0, Hs);
+
+            A.setSubMatrix(size, 0, Wq);
+            A.setSubMatrix(0, size, Wq.transpose());
+
+
+/// LU Solver
+            // 'cublas' LU Solver !!!!
+            LUDecompositionQuick LU = new LUDecompositionQuick();
+            LU.decompose(matrix2DA);
+            LU.solve(matrix1Db);
+
+
+            // `cublas add  -- y = x * alfa + beta`
+            SV.add(dmx);
+            PointUtility.copyFromStateVector(SV); /// tylko na zakonczenie syncronizacja do CPU-ram
+
+            // `kernel` + `cublas`
+            norm1 = Constraint.getFullNorm();
+
+            // *&double
+            if (norm1 < CONVERGENCE_LIMIT) {
+                solverStat.error = norm1;
+                reporter.writeln("");
+                break;
+            }
+
+            if (itr > 1 && errorFluctuation / prevNorm > 0.70) {
+
+                /// Native - wypelnic - native getSolverState()
+                solverStat.report(reporter);
+
+                return null; // SUCCESS, ERROR
+                return solverStat;
+            }
+            itr++;
+        }
+
+        ///     } : C20
+
+        // JNI:
+        // native getErrorCode()
+        // native getErrorMessage()
+        // native getSolverState()
+
+        // native SolverState getSolverState()
+        solverStat.report(reporter);
+        return solverStat;
+    }
+
+
+}
+
+*/
---
+++ src/main/java/com/mstruzek/msketch/solver/jni/JniSolverGateway.java
@@ -0,0 +1,49 @@
+package com.mstruzek.msketch.solver.jni;
+
+public class JniSolverGateway {
+
+    /**
+     * Register new or resize actual shared buffer for internal communication.
+     * @param size byte buffer capacity
+     * @return address ? UNSAFE set address
+     */
+    native long registerBuffer(long size);
+
+    native long freeBuffer(long address);
+
+
+    /**
+     * Serialize object into shared buffer in Java, and deserializer on the other side.
+     */
+    native void readObject(long type, int id);
+
+    /**
+     * Serialize object into shared buffer, and deserialize in Java.
+     */
+    native void writeObject(long type, int id);
+
+    /**
+     * Run single execution function identified by Id.
+     * @return 0 on success otherwise read error status.
+     */
+    native int execute(int action);
+
+    /**
+     * Error should be registered for system invalid actions, failures of  cuda API, failures of  CUBLAS invocation ?.
+     * @return 1: system error, 2: GPU API, 4: CUBLAS API
+     */
+    native int errorType();
+
+    /**
+     * Get last registered error code from application or kernel space.
+     * @return error code , depends on source space.
+     */
+    native int errorCode();
+
+    /**
+     * Get last error byte buffer message. Byte array initialized in JVM arena.
+     * @return UTF-8 encoded string ( ,? UTF-16 )
+     */
+    native byte[] errorMessage();
+
+}
