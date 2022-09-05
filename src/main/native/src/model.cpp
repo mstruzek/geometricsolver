1ef1d924cd6e5cc68aba188d58f0575a0a780226
michal struzek <mstruzek@>
2022-09-05 13:43:51 +0200

    coo format requirments of model configuration

--- src/main/native/src/model.cpp
+++ src/main/native/src/model.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-namespace graph { 
+namespace graph {
 
 int constraintSize(Constraint const &constraint) {
     switch (constraint.constraintTypeId) {
@@ -60,6 +60,87 @@ int geometricSetSize(Geometric const &geometric) {
         return 7 * 2;
     default:
         printf("unknown geometric type");
+        exit(1);
+    }
+}
+
+///
+/// __device__ __host__ COO tensor format requirments
+///
+int tensorOpsCooStiffnesCoefficients(Geometric const &geometric) {
+    switch (geometric.geometricTypeId) {
+    case GEOMETRIC_TYPE_ID_FREE_POINT:
+        return 7 * 2; // 7 - plusSubTensor  * diagonal I (2)
+    case GEOMETRIC_TYPE_ID_LINE:
+        return 10 * 2; // 10 - plusSubTensor * diagonal I (2)
+    case GEOMETRIC_TYPE_ID_CIRCLE:
+        return 10 * 2; // 10 - plusSubTensor * diagonal I (2)
+    case GEOMETRIC_TYPE_ID_ARC:
+        return 19 * 2; // 19 - plusSubTensor * diagonal I (2)
+    default:
+        printf("unknown geometric type");
+        exit(1);
+    }
+}
+
+///
+/// __device__ __host__ ??  ------ COO tensor format requirments --> exclusive_scan --> cudaMalloc(accWriteOffset)
+///
+int tensorOpsCooConstraintJacobian(Constraint const &constraint) {
+    switch (constraint.constraintTypeId) {
+    case CONSTRAINT_TYPE_ID_FIX_POINT:
+        /// 1 * diagonal (2)
+        return 2;
+    case CONSTRAINT_TYPE_ID_PARAMETRIZED_XFIX:
+        ///  1 * quick
+        return 1;
+    case CONSTRAINT_TYPE_ID_PARAMETRIZED_YFIX:
+        ///  1 * quick
+        return 1;
+    case CONSTRAINT_TYPE_ID_CONNECT_2_POINTS:
+        /// 2 * diagonal (2)
+        return 4;
+    case CONSTRAINT_TYPE_ID_HORIZONTAL_POINT:
+        /// 2 * quick
+        return 2;
+    case CONSTRAINT_TYPE_ID_VERTICAL_POINT:
+        ///  2 * quick
+        return 2;
+    case CONSTRAINT_TYPE_ID_LINES_PARALLELISM:
+        /// 4 * vector(2)
+        return 8;
+    case CONSTRAINT_TYPE_ID_LINES_PERPENDICULAR:
+        /// 4 * vector(2)
+        return 8;
+    case CONSTRAINT_TYPE_ID_EQUAL_LENGTH:
+        /// 4 * vector(2)
+        return 8;
+    case CONSTRAINT_TYPE_ID_PARAMETRIZED_LENGTH:
+        /// 4 * vector(2)
+        return 8;
+    case CONSTRAINT_TYPE_ID_TANGENCY:
+        /// 4 * vector(2)
+        return 8;
+    case CONSTRAINT_TYPE_ID_CIRCLE_TANGENCY:
+        /// 4 * vector(2)
+        return 8;
+    case CONSTRAINT_TYPE_ID_DISTANCE_2_POINTS:
+        /// 2 * vector(2)
+        return 4;        
+    case CONSTRAINT_TYPE_ID_DISTANCE_POINT_LINE:
+        /// 3 * vector(2)
+        return 6;
+    case CONSTRAINT_TYPE_ID_ANGLE_2_LINES:
+        /// 4 * vector(2)
+        return 8;
+    case CONSTRAINT_TYPE_ID_SET_HORIZONTAL:
+        /// 2 * vector(2)
+        return 4;
+    case CONSTRAINT_TYPE_ID_SET_VERTICAL:
+        /// 2 * vector(2)
+        return 4;
+    default:
+        printf("unknown constraint type");
         exit(1);
     }
 }
